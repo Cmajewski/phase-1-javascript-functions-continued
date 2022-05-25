53e45d8b2d92421d9613028088444f9df2db1c89
github username <github email address>
2022-05-24 22:09:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const saturdayFun=function(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork=function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective=function(type){
+   return function(activity){
+       return(`You are ${type}${activity}${type}!`)
+   }
+   
+
+}
+
